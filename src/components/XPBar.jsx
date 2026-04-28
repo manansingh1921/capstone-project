493b5ba6009fb08a148122e38e810c3f6ec302c9
Manan <manansingh1921@gmail.com>
2026-04-29 01:01:07 +0530

    Add XPBar component to display experience progress

--- src/components/XPBar.jsx
+++ src/components/XPBar.jsx
@@ -1,1 +1,8 @@
-
+export default function XPBar({ xp }) {
+  const percent = xp % 100;
+  return (
+    <div className="xp-bar">
+      <div className="xp-fill" style={{ width: `${percent}%` }} />
+    </div>
+  );
+}
